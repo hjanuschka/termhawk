b922ba3cd2adf413db7fbf9efc49f77194e9eb75
Helmut Januschka <helmut@januschka.com>
2018-01-25 12:44:40 +0100

    Update bottombar.js

--- bottombar.js
+++ bottombar.js
@@ -29,7 +29,7 @@ class BottomBar {
                 item: {
                     bg: 'magenta',
                     fg: 'white'
-                    //focus: {
+                    //focus: {asd asda
                     //  bg: 'blue'
                     //}
                 },
