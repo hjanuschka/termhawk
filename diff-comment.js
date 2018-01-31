ce8a7d5faca7b1bb7b6a2804f25fee5a855b4b5b
Helmut Januschka <helmut@januschka.com>
2018-01-31 23:00:34 +0100

    up

--- diff-comment.js
+++ diff-comment.js
@@ -4,9 +4,109 @@ var DiffComment = function(options) {
         return new DiffComment(options)
     }
 
+    var self = this
     options = options || {}
     options.shrink = true
     Blessed.Element.call(this, options)
+
+    if (options.keys) {
+        this.on('keypress', function(ch, key) {
+            if (key.name === 'up' || (options.vi && key.name === 'k')) {
+                self.up()
+                self.screen.render()
+                return
+            }
+            if (key.name === 'down' || (options.vi && key.name === 'j')) {
+                self.down()
+                self.screen.render()
+                return
+            }
+            if (key.name === 'enter' ||
+                (options.vi && key.name === 'l' && !key.shift)) {
+                self.enterSelected()
+                return
+            }
+            if (key.name === 'escape' || (options.vi && key.name === 'q')) {
+                self.cancelSelected()
+                return
+            }
+            if (options.vi && key.name === 'u' && key.ctrl) {
+                self.move(-((self.height - self.iheight) / 2) | 0)
+                self.screen.render()
+                return
+            }
+            if (options.vi && key.name === 'd' && key.ctrl) {
+                self.move((self.height - self.iheight) / 2 | 0)
+                self.screen.render()
+                return
+            }
+            if (options.vi && key.name === 'b' && key.ctrl) {
+                self.move(-(self.height - self.iheight))
+                self.screen.render()
+                return
+            }
+            if (options.vi && key.name === 'f' && key.ctrl) {
+                self.move(self.height - self.iheight)
+                self.screen.render()
+                return
+            }
+            if (options.vi && key.name === 'h' && key.shift) {
+                self.move(self.childBase - self.selected)
+                self.screen.render()
+                return
+            }
+            if (options.vi && key.name === 'm' && key.shift) {
+                // TODO: Maybe use Math.min(this.items.length,
+                // ... for calculating visible items elsewhere.
+                var visible = Math.min(
+                    self.height - self.iheight,
+                    self.items.length) / 2 | 0
+                self.move(self.childBase + visible - self.selected)
+                self.screen.render()
+                return
+            }
+            if (options.vi && key.name === 'l' && key.shift) {
+                // XXX This goes one too far on lists with an odd number of items.
+                self.down(self.childBase +
+                    Math.min(self.height - self.iheight, self.items.length) -
+                    self.selected)
+                self.screen.render()
+                return
+            }
+            if (options.vi && key.name === 'g' && !key.shift) {
+                self.select(0)
+                self.screen.render()
+                return
+            }
+            if (options.vi && key.name === 'g' && key.shift) {
+                self.select(self.items.length - 1)
+                self.screen.render()
+                return
+            }
+
+            if (options.vi && (key.ch === '/' || key.ch === '?')) {
+                if (typeof self.options.search !== 'function') {
+                    return
+                }
+                return self.options.search(function(err, value) {
+                    if (typeof err === 'string' || typeof err === 'function' ||
+                        typeof err === 'number' || (err && err.test)) {
+                        value = err
+                        err = null
+                    }
+                    if (err || !value) return self.screen.render()
+                    self.select(self.fuzzyFind(value, key.ch === '?'))
+                    self.screen.render()
+                })
+            }
+        })
+    }
+}
+
+DiffComment.prototype.enterSelected = function(i) {
+    if (i != null) this.select(i)
+    this.emit('action', this.items[this.selected], this.selected)
+    this.emit('select', this.items[this.selected], this.selected)
 }
 
 DiffComment.prototype = Object.create(Blessed.Element.prototype)
@@ -14,7 +114,7 @@ DiffComment.prototype.type = 'diff-comment'
 
 // Define a instance property called `messages` that will store the message list.
 DiffComment.prototype._lines = []
-DiffComment.prototype._messages = ["a","b"]
+DiffComment.prototype._messages = ['a', 'b']
 DiffComment.prototype._selectedLine = 0
 DiffComment.prototype._selectedPath = ''
 
@@ -24,7 +124,7 @@ DiffComment.prototype._selectedPath = ''
  */
 DiffComment.prototype.setDiff = function(diff) {
     var self = this
-    self._lines = diff.split("\n")
+    self._lines = diff.split('\n')
 
 
 }
--- diff-test.js
+++ diff-test.js
@@ -8,15 +8,83 @@ var screen = Blessed.screen({
 })
 
 var diff = require('./diff-comment')
-var d = DiffComment({
+var d = Blessed.list({
     height: '100%',
     padding: 1,
     tags: true,
     border: 'line',
-    width: '100%',
-    parent: screen
+    width: '40%',
+    parent: screen,
+    keys: true,
+    //width: '80%',
+    vi: true,
+    mouse: true,
+    style: {
+        border: {
+            fg: 'red'
+        },
+        header: {
+            fg: 'blue',
+            bold: true
+        },
+        selected: {
+            bg: 'blue'
+        }
+    }
 })
 
+function diffPosition(lines, offset) {
+    var line_to_find = lines[offset]
+    //Loop Up to Find diff
+    var i = 0
+    var pathname = ''
+    var file_start = 0
+    for (i = offset; i >= 0; i--) {
+        var matches = lines[i].match(/^diff \-\-git a\/(.*) b\//)
+        if (matches) {
+            pathname = matches[1]
+            file_start = i
+            break
+        }
+    }
+    var hunk_start = 0
+    for (i = file_start; i < lines.length - 1; i++) {
+        if (lines[i].match(/^@@/)) {
+            hunk_start = i + 1
+            break
+        }
+    }
+    var found_line_nr = 1
+    for (i = hunk_start; i < lines.length - 1; i++) {
+
+        if (lines[i] == line_to_find) {
+            break
+        }
+        found_line_nr++
+    }
+    var finalPayload = {
+        pathname: pathname,
+        nr: found_line_nr
+    }
+    return finalPayload
+
+
+
+}
+
 var diff_data = fs.readFileSync('./demo.diff', 'utf8')
-d.setDiff(diff_data.toString())
+//d.setDiff(diff_data.toString())
+var lines = diff_data.toString().split('\n')
+lines.forEach(function(l) {
+    d.addItem(l)
+})
+d.focus()
+d.on('select', function(item, idx) {
+    var found = diffPosition(lines, idx)
+    console.log(found)
+})
+
+screen.key('q', function() {
+    return screen.destroy()
+})
 screen.render()
